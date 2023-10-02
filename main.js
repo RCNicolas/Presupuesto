5d36d586b781272dfd62810cd30127772b14f9c7
RCNicolas <114864432+RCNicolas@users.noreply.github.com>
2023-10-01 23:24:12 -0500

    feat: :sparkles: Formateo y totales
    Se agrea el formato de pesos al valor de los productos
    se agregan los totales de ingreso, egreso y el total general de los productos
    
    @CampusLands

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
+//main.js
 const dialog = document.querySelector("dialog");
 const myFromAdd = document.querySelector("#addForm");
 const editForm = document.querySelector("#editForm");
 const myTabla = document.querySelector("#myData");
 const urlAPI = "http://localhost:3000/presupestos";
-
-// ! Funcion para eliminar
-// !-----------------------------------------------------------------------
+let totalIngresos = 0;
+let totalEgresos = 0;
+let total = 0;
+// Funcion para eliminar
 const deleteData = async (id) => {
   let config = {
     method: "DELETE",
@@ -14,8 +16,7 @@ const deleteData = async (id) => {
   let res = await (await fetch(urlAPI + "/" + id, config)).json();
 };
 
-// ! Guardar data en el API
-// !-----------------------------------------------------------------------
+// Guardar data en el API
 myFromAdd.addEventListener("submit", async (e) => {
   e.preventDefault();
   const data = Object.fromEntries(new FormData(e.target));
@@ -35,21 +36,19 @@ myFromAdd.addEventListener("submit", async (e) => {
       const res = await response.json();
       console.log(res);
     } else {
-      console.log("Error al guardar los data. ", response.stringify);
+      console.log("Error al guardar los datos. ", response.stringify);
     }
   } catch (error) {
-    console.log("Error al guardar los data. ", response.stringify);
+    console.log("Error al guardar los datos. ", error);
   }
 });
 
-// ! Funcion para editar
-// !-----------------------------------------------------------------------
+// Funcion para editar
 const editData = async (id) => {
-  dialog.showModal(); // Mostrar el dialog
+  dialog.showModal();
   const response = await fetch(urlAPI + "/" + id);
   const data = await response.json();
 
-  // definir las variables de edicion
   const editValor = document.querySelector("#editValor");
   const editCajaIngreso = document.querySelector("#editCajaIngreso");
   const editCajaEgreso = document.querySelector("#editCajaEgreso");
@@ -64,14 +63,14 @@ const editData = async (id) => {
 
   editCantidad.value = data.cantidad;
   editProducto.value = data.producto;
-  // ? Función para el manejo de envio del formulario de edición
+
   const submitEditForm = async (e) => {
     e.preventDefault();
     const updateData = Object.fromEntries(new FormData(e.target));
-    // Convertir a numericos los valores cambiados en la edición 
     const { valor, cantidad } = updateData;
     updateData.valor = typeof valor === "string" ? Number(valor) : null;
-    updateData.cantidad = typeof cantidad === "string" ? Number(cantidad) : null;
+    updateData.cantidad =
+      typeof cantidad === "string" ? Number(cantidad) : null;
 
     updateData.caja = editCajaIngreso.checked ? "Ingreso" : "Egreso";
 
@@ -82,17 +81,15 @@ const editData = async (id) => {
     };
 
     try {
-      const response = await fetch(urlAPI + "/" + id, config); //Solicitut para actualización de data
+      const response = await fetch(urlAPI + "/" + id, config);
 
       response.ok
         ? dialog.close()
-        : console.log("Error al actualizar data. ", response.statusText);
+        : console.log("Error al actualizar los datos. ", response.statusText);
     } catch (error) {
-      console.log("Error en la edicion de los data. ");
+      console.log("Error en la edición de los datos. ", error);
     }
   };
-
-  // ? Manejo de eventos del envio del formulario de edición
 
   editForm.addEventListener("submit", submitEditForm);
 
@@ -101,8 +98,7 @@ const editData = async (id) => {
   });
 };
 
-// ! Funcion para realizar la busqueda
-// !-----------------------------------------------------------------------
+// Función para realizar la búsqueda
 document.querySelector("#searchButton").addEventListener("click", () => {
   const searchTerm = document
     .querySelector("#searchProduct")
@@ -112,22 +108,31 @@ document.querySelector("#searchButton").addEventListener("click", () => {
   rows.forEach((row) => {
     const productoCell = row
       .querySelector("[data-producto]")
-      .getAttribute("data-producto")
-      .toLowerCase();
+      .getAttribute("data-producto");
 
-    productoCell.includes(searchTerm)
-      ? (row.style.display = "")
-      : (row.style.display = "none");
+    if (productoCell) {
+      const productoLower = productoCell.toLowerCase();
+      if (productoLower.includes(searchTerm)) {
+        row.style.display = "";
+      } else {
+        row.style.display = "none";
+      }
+    }
   });
 });
 
-// ! Función para mostrar la tabla
-// !-----------------------------------------------------------------------
+// Función para formatear los precios
+const formatPrice = (price) => {
+  return new Intl.NumberFormat("de-DE", {
+    style: "currency",
+    currency: "COP",
+  }).format(price);
+};
 
+// Función para mostrar la tabla
 addEventListener("DOMContentLoaded", async () => {
   try {
     let res = await (await fetch(urlAPI)).json();
-    // myTabla.innerHTML = "";
 
     for (let i = 0; i < res.length; i++) {
       myTabla.insertAdjacentHTML(
@@ -135,17 +140,33 @@ addEventListener("DOMContentLoaded", async () => {
         `
         <tr class="row" id="${res[i].id}">
           <td>${i + 1}</td>
-          <td data-valor="${res[i].valor}">${res[i].valor}</td>
+          <td data-valor="${res[i].valor}">$ ${formatPrice(res[i].valor)}</td>
           <td data-caja="${res[i].caja}">${res[i].caja}</td>
           <td data-cantidad="${res[i].cantidad}">${res[i].cantidad}</td>
           <td data-producto="${res[i].producto}">${res[i].producto}</td>
-          <td>
+          <td class="button-container"> <!-- Nuevo contenedor para los botones -->
             <button id="${res[i].id}" class="delete">Eliminar</button>
             <button id="${res[i].id}" class="edit">Editar</button>
           </td>
         </tr>`
       );
+
+      // Actualizar totales
+      if (res[i].caja === "ingreso") {
+        totalIngresos += res[i].valor;
+      } else if (res[i].caja === "egreso") {
+        totalEgresos += res[i].valor;
+      }
+      total = totalIngresos - totalEgresos;
     }
+
+    // Mostrar totales
+    document.querySelector("#totalIngresos").textContent =
+      formatPrice(totalIngresos);
+    document.querySelector("#totalEgresos").textContent =
+      formatPrice(totalEgresos);
+    document.querySelector("#total").textContent =
+      formatPrice(total);
 
     document.querySelectorAll(".delete").forEach((button) => {
       button.addEventListener("click", () => {
@@ -164,3 +185,44 @@ addEventListener("DOMContentLoaded", async () => {
     console.error("Error al cargar los datos:", error);
   }
 });
+
+// Función para mostrar la tabla
+// addEventListener("DOMContentLoaded", async () => {
+//   try {
+//     let res = await (await fetch(urlAPI)).json();
+
+//     for (let i = 0; i < res.length; i++) {
+//       myTabla.insertAdjacentHTML(
+//         "beforeend",
+//         `
+//         <tr class="row" id="${res[i].id}">
+//           <td>${i + 1}</td>
+//           <td data-valor="${res[i].valor}">$ ${formatPrice(res[i].valor)}</td>
+//           <td data-caja="${res[i].caja}">${res[i].caja}</td>
+//           <td data-cantidad="${res[i].cantidad}">${res[i].cantidad}</td>
+//           <td data-producto="${res[i].producto}">${res[i].producto}</td>
+//           <td class="button-container">
+//             <button id="${res[i].id}" class="delete">Eliminar</button>
+//             <button id="${res[i].id}" class="edit">Editar</button>
+//           </td>
+//         </tr>`
+//       );
+//     }
+
+//     document.querySelectorAll(".delete").forEach((button) => {
+//       button.addEventListener("click", () => {
+//         const id = button.id;
+//         deleteData(id);
+//       });
+//     });
+
+//     document.querySelectorAll(".edit").forEach((button) => {
+//       button.addEventListener("click", () => {
+//         const id = button.id;
+//         editData(id);
+//       });
+//     });
+//   } catch (error) {
+//     console.error("Error al cargar los datos:", error);
+//   }
+// });
